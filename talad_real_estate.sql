5c7db26f6e451b1f920ba5bf859e50ab32e9a054
Asimtechcampus <spshal6@gmail.com>
2024-10-04 07:30:21 +0300

    إضافة الخريطة لتفاصيل المشروع

--- talad_real_estate.sql
+++ talad_real_estate.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 02 أكتوبر 2024 الساعة 20:03
+-- Generation Time: 04 أكتوبر 2024 الساعة 03:53
 -- إصدار الخادم: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- بنية الجدول `attachments`
+--
+
+CREATE TABLE `attachments` (
+  `id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `attachment_type` enum('مبيعات','عميل') NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- إرجاع أو استيراد بيانات الجدول `attachments`
+--
+
+INSERT INTO `attachments` (`id`, `client_id`, `attachment_type`, `name`, `path`, `created_at`) VALUES
+(1, 1, 'مبيعات', 'بطاقة الهوية', '../uploads/sadn.png', '2024-10-02 22:33:46');
+
+-- --------------------------------------------------------
+
+--
 -- بنية الجدول `calls`
 --
 
@@ -34,6 +56,30 @@ CREATE TABLE `calls` (
   `call_count` int(11) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `client_tasks`
+--
+
+CREATE TABLE `client_tasks` (
+  `id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `task_type` varchar(50) NOT NULL,
+  `task_date` date NOT NULL,
+  `task_time` time NOT NULL,
+  `description` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- إرجاع أو استيراد بيانات الجدول `client_tasks`
+--
+
+INSERT INTO `client_tasks` (`id`, `client_id`, `task_type`, `task_date`, `task_time`, `description`, `created_at`) VALUES
+(1, 1, 'اجتماع', '2024-10-05', '03:00:00', 'لا اعرف', '2024-10-02 21:57:39'),
+(2, 1, 'زيارة', '2024-10-12', '05:32:00', 'fdsd', '2024-10-02 22:32:55');
 
 -- --------------------------------------------------------
 
@@ -92,15 +138,19 @@ CREATE TABLE `customers` (
   `sector` varchar(100) DEFAULT NULL,
   `assigned_to` int(11) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `source` text NOT NULL
+  `source` text NOT NULL,
+  `notes` varchar(128) DEFAULT NULL,
+  `status` varchar(128) DEFAULT NULL,
+  `contact_date` date DEFAULT NULL,
+  `salary` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- إرجاع أو استيراد بيانات الجدول `customers`
 --
 
-INSERT INTO `customers` (`id`, `name`, `email`, `phone`, `monthly_commitment`, `bank`, `sector`, `assigned_to`, `created_at`, `source`) VALUES
-(1, 'عميل طازه', 'asim1a@hotmail.com', '9999999', 1000.00, 'الراجحي', 'حكومي', 3, '2024-09-25 16:44:43', '');
+INSERT INTO `customers` (`id`, `name`, `email`, `phone`, `monthly_commitment`, `bank`, `sector`, `assigned_to`, `created_at`, `source`, `notes`, `status`, `contact_date`, `salary`) VALUES
+(1, 'عميل طازه', 'asim1a@hotmail.com', '9999999', 1000.00, 'الراجحي', 'حكومي', 3, '2024-09-25 16:44:43', '', NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -165,7 +215,8 @@ CREATE TABLE `potential_clients` (
 INSERT INTO `potential_clients` (`id`, `name`, `phone`, `salary`, `monthly_commitment`, `bank`, `sector`, `status`, `notes`, `contact_date`, `assigned_to`, `created_at`, `email`, `source`) VALUES
 (30, 'عميل جديد', '059999999', 10000.00, 2000.00, 'الراجحي', 'عسكري', 'مهتم', 'لايوجد', '0000-00-00', 3, '2024-09-24 19:05:43', 'engineer@engineer.com', ''),
 (31, 'احمد', '0999599993', 10000.00, 200.00, 'البنك', 'عسكري', 'مهتم', 'مهتم', '0000-00-00', 3, '2024-09-25 00:04:10', 'asim1a@hotmail.com', ''),
-(32, 'عميل طازه', '9999999', 40000.00, 1000.00, 'الراجحي', 'حكومي', 'تم البيع', 'جديد', '0000-00-00', 3, '2024-09-25 13:31:12', 'asim1a@hotmail.com', '');
+(32, 'عميل طازه', '9999999', 40000.00, 1000.00, 'الراجحي', 'حكومي', 'تم البيع', 'جديدث', '0000-00-00', 3, '2024-09-25 13:31:12', 'asim1a@hotmail.com', ''),
+(35, 'Asim Alzubaidi', '0599115017', 12000.00, 1253.00, 'الراجحي', 'عسكري', 'جديد', 'بدون تحديثات', '2024-10-16', 3, '2024-10-02 20:17:10', 'asim1a@hotmail.com', 'الهاتف');
 
 -- --------------------------------------------------------
 
@@ -214,7 +265,9 @@ INSERT INTO `projects` (`id`, `name`, `city`, `logo`, `total_units`, `sold_units
 (18, 'مشروع جدة', '', NULL, 0, 0, 0, '2024-09-22 21:28:56', 'مجمع تجاري في وسط جدة', '2023-03-15', '2025-06-30', ''),
 (19, 'مشروع الدمام', '', NULL, 0, 0, 0, '2024-09-22 21:28:56', 'برج سكني في الدمام', '2023-05-01', '2024-11-30', ''),
 (20, 'مشروعي', 'جدة', '', 599, 0, 4, '2024-09-27 11:37:32', 'note\n', NULL, NULL, 'planned'),
-(21, 'مشروع ضاحي بن خلفان', 'جدة', '', 4000, 0, 9, '2024-09-27 11:43:10', NULL, NULL, NULL, 'planned');
+(21, 'مشروع ضاحي بن خلفان', 'جدة', '', 4000, 0, 9, '2024-09-27 11:43:10', NULL, NULL, NULL, ''),
+(22, 'عاصم', 'جدة', NULL, 499, NULL, 4, '2024-10-03 21:20:03', 'بدون وصف', '2024-10-04', '2024-10-16', 'planned'),
+(23, 'KSA', 'مكة', 'sadn.png', 455, 0, 3, '2024-10-03 21:32:41', 'بدون', '2024-10-04', '2027-07-04', 'in_progress');
 
 --
 -- القوادح `projects`
@@ -225,6 +278,195 @@ CREATE TRIGGER `update_remaining_units` BEFORE UPDATE ON `projects` FOR EACH ROW
 END
 $$
 DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `project_property_types`
+--
+
+CREATE TABLE `project_property_types` (
+  `id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `property_type_id` int(11) NOT NULL,
+  `available_units` int(11) DEFAULT 0,
+  `quantity` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- إرجاع أو استيراد بيانات الجدول `project_property_types`
+--
+
+INSERT INTO `project_property_types` (`id`, `project_id`, `property_type_id`, `available_units`, `quantity`) VALUES
+(1, 21, 3, 4, 0),
+(2, 21, 2, 4, 0),
+(3, 21, 1, 4, 0),
+(4, 20, 3, 0, 3),
+(5, 20, 2, 0, 6),
+(6, 20, 1, 0, 7),
+(7, 22, 3, 0, 2),
+(8, 22, 2, 0, 2),
+(9, 22, 1, 0, 2),
+(10, 20, 3, 0, 10),
+(11, 20, 2, 0, 2),
+(12, 20, 1, 0, 5),
+(13, 12, 3, 0, 10),
+(14, 12, 2, 0, 2),
+(15, 12, 1, 0, 4),
+(16, 8, 3, 0, 5),
+(17, 8, 2, 0, 5),
+(18, 8, 1, 0, 5);
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `properties`
+--
+
+CREATE TABLE `properties` (
+  `id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `property_type_id` int(11) NOT NULL,
+  `serial_number` varchar(4) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `status` enum('متاح','محجوز','تم البيع') NOT NULL,
+  `readiness` enum('لم يتم البدء','تحت الانشاء','مكتمل') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- إرجاع أو استيراد بيانات الجدول `properties`
+--
+
+INSERT INTO `properties` (`id`, `project_id`, `property_type_id`, `serial_number`, `price`, `status`, `readiness`) VALUES
+(1, 22, 1, '9181', 0.00, 'محجوز', ''),
+(2, 22, 1, '4019', 0.00, 'متاح', ''),
+(3, 22, 2, '2865', 0.00, 'متاح', ''),
+(4, 22, 2, '2403', 0.00, 'متاح', 'لم يتم البدء'),
+(5, 22, 3, '7936', 0.00, 'متاح', 'لم يتم البدء'),
+(6, 22, 3, '5525', 0.00, 'متاح', 'لم يتم البدء'),
+(7, 22, 1, '7863', 0.00, 'متاح', 'لم يتم البدء'),
+(8, 22, 1, '3151', 0.00, 'متاح', 'لم يتم البدء'),
+(9, 22, 2, '1042', 0.00, 'متاح', 'لم يتم البدء'),
+(10, 22, 2, '1787', 0.00, 'متاح', 'لم يتم البدء'),
+(11, 22, 3, '1762', 0.00, 'متاح', 'لم يتم البدء'),
+(12, 22, 3, '9686', 0.00, 'متاح', 'لم يتم البدء'),
+(13, 20, 3, '7402', 0.00, 'متاح', 'لم يتم البدء'),
+(14, 20, 3, '6332', 0.00, 'متاح', 'لم يتم البدء'),
+(15, 20, 3, '3601', 0.00, 'متاح', 'لم يتم البدء'),
+(16, 20, 2, '7754', 0.00, 'متاح', 'لم يتم البدء'),
+(17, 20, 2, '3710', 0.00, 'متاح', 'لم يتم البدء'),
+(18, 20, 2, '5600', 0.00, 'متاح', 'لم يتم البدء'),
+(19, 20, 2, '3659', 0.00, 'متاح', 'لم يتم البدء'),
+(20, 20, 2, '9131', 0.00, 'متاح', 'لم يتم البدء'),
+(21, 20, 2, '7247', 0.00, 'متاح', 'لم يتم البدء'),
+(22, 20, 1, '4130', 0.00, 'متاح', 'لم يتم البدء'),
+(23, 20, 1, '9563', 0.00, 'متاح', 'لم يتم البدء'),
+(24, 20, 1, '3400', 0.00, 'متاح', 'لم يتم البدء'),
+(25, 20, 1, '5801', 0.00, 'متاح', 'لم يتم البدء'),
+(26, 20, 1, '9598', 0.00, 'متاح', 'لم يتم البدء'),
+(27, 20, 1, '4650', 0.00, 'متاح', 'لم يتم البدء'),
+(28, 20, 1, '9912', 0.00, 'متاح', 'لم يتم البدء'),
+(29, 20, 3, '5256', 0.00, 'متاح', 'لم يتم البدء'),
+(30, 20, 3, '0044', 0.00, 'متاح', 'لم يتم البدء'),
+(31, 20, 3, '9446', 0.00, 'متاح', 'لم يتم البدء'),
+(32, 20, 3, '5450', 0.00, 'متاح', 'لم يتم البدء'),
+(33, 20, 3, '7307', 0.00, 'متاح', 'لم يتم البدء'),
+(34, 20, 3, '0965', 0.00, 'متاح', 'لم يتم البدء'),
+(35, 20, 3, '6472', 0.00, 'متاح', 'لم يتم البدء'),
+(36, 20, 3, '0788', 0.00, 'متاح', 'لم يتم البدء'),
+(37, 20, 3, '1725', 0.00, 'متاح', 'لم يتم البدء'),
+(38, 20, 3, '5442', 0.00, 'متاح', 'لم يتم البدء'),
+(39, 20, 2, '1259', 0.00, 'متاح', 'لم يتم البدء'),
+(40, 20, 2, '6593', 0.00, 'متاح', 'لم يتم البدء'),
+(41, 20, 1, '3348', 0.00, 'متاح', 'لم يتم البدء'),
+(42, 20, 1, '7262', 0.00, 'متاح', 'لم يتم البدء'),
+(43, 20, 1, '3689', 0.00, 'متاح', 'لم يتم البدء'),
+(44, 20, 1, '1566', 0.00, 'متاح', 'لم يتم البدء'),
+(45, 20, 1, '1320', 0.00, 'متاح', 'لم يتم البدء'),
+(46, 12, 1, '1755', 0.00, 'متاح', 'لم يتم البدء'),
+(47, 12, 1, '0669', 0.00, 'متاح', 'لم يتم البدء'),
+(48, 12, 1, '5680', 0.00, 'متاح', 'لم يتم البدء'),
+(49, 12, 1, '8279', 0.00, 'متاح', 'لم يتم البدء'),
+(50, 12, 2, '8409', 0.00, 'متاح', 'لم يتم البدء'),
+(51, 12, 2, '1863', 0.00, 'متاح', 'لم يتم البدء'),
+(52, 12, 3, '1598', 0.00, 'متاح', 'لم يتم البدء'),
+(53, 12, 3, '3712', 0.00, 'متاح', 'لم يتم البدء'),
+(54, 12, 3, '5718', 0.00, 'متاح', 'لم يتم البدء'),
+(55, 12, 3, '7933', 0.00, 'متاح', 'لم يتم البدء'),
+(56, 12, 3, '8142', 0.00, 'متاح', 'لم يتم البدء'),
+(57, 12, 3, '5251', 0.00, 'متاح', 'لم يتم البدء'),
+(58, 12, 3, '8004', 0.00, 'متاح', 'لم يتم البدء'),
+(59, 12, 3, '7482', 0.00, 'متاح', 'لم يتم البدء'),
+(60, 12, 3, '9763', 0.00, 'متاح', 'لم يتم البدء'),
+(61, 12, 3, '2291', 0.00, 'متاح', 'لم يتم البدء'),
+(62, 8, 1, '4305', 0.00, 'متاح', 'لم يتم البدء'),
+(63, 8, 1, '3394', 0.00, 'متاح', 'لم يتم البدء'),
+(64, 8, 1, '6359', 0.00, 'متاح', 'لم يتم البدء'),
+(65, 8, 1, '1594', 0.00, 'متاح', 'لم يتم البدء'),
+(66, 8, 1, '8015', 0.00, 'متاح', 'لم يتم البدء'),
+(67, 8, 2, '0752', 0.00, 'متاح', 'لم يتم البدء'),
+(68, 8, 2, '8833', 0.00, 'متاح', 'لم يتم البدء'),
+(69, 8, 2, '1930', 0.00, 'متاح', 'لم يتم البدء'),
+(70, 8, 2, '4393', 0.00, 'متاح', 'لم يتم البدء'),
+(71, 8, 2, '6429', 0.00, 'متاح', 'لم يتم البدء'),
+(72, 8, 3, '2448', 0.00, 'متاح', 'لم يتم البدء'),
+(73, 8, 3, '3129', 0.00, 'متاح', 'لم يتم البدء'),
+(74, 8, 3, '2031', 0.00, 'متاح', 'لم يتم البدء'),
+(75, 8, 3, '0568', 0.00, 'متاح', 'لم يتم البدء'),
+(76, 8, 3, '7001', 0.00, 'متاح', 'لم يتم البدء'),
+(77, 8, 1, '3044', 0.00, 'متاح', 'لم يتم البدء'),
+(78, 8, 1, '7142', 0.00, 'متاح', 'لم يتم البدء'),
+(79, 8, 1, '0477', 0.00, 'متاح', 'لم يتم البدء'),
+(80, 8, 1, '6726', 0.00, 'متاح', 'لم يتم البدء'),
+(81, 8, 1, '2339', 0.00, 'متاح', 'لم يتم البدء'),
+(82, 8, 2, '4610', 0.00, 'متاح', 'لم يتم البدء'),
+(83, 8, 2, '0511', 0.00, 'متاح', 'لم يتم البدء'),
+(84, 8, 2, '5831', 0.00, 'متاح', 'لم يتم البدء'),
+(85, 8, 2, '3833', 0.00, 'متاح', 'لم يتم البدء'),
+(86, 8, 2, '5693', 0.00, 'متاح', 'لم يتم البدء'),
+(87, 8, 3, '7842', 0.00, 'متاح', 'لم يتم البدء'),
+(88, 8, 3, '5245', 0.00, 'متاح', 'لم يتم البدء'),
+(89, 8, 3, '0001', 0.00, 'متاح', 'لم يتم البدء'),
+(90, 8, 3, '6724', 0.00, 'متاح', 'لم يتم البدء'),
+(91, 8, 3, '6620', 0.00, 'متاح', 'لم يتم البدء'),
+(92, 22, 1, '9347', 0.00, 'متاح', 'لم يتم البدء'),
+(93, 22, 1, '0006', 0.00, 'متاح', 'لم يتم البدء'),
+(94, 22, 2, '0799', 0.00, 'متاح', 'لم يتم البدء'),
+(95, 22, 2, '0461', 0.00, 'متاح', 'لم يتم البدء'),
+(96, 22, 3, '6085', 0.00, 'متاح', 'لم يتم البدء'),
+(97, 22, 3, '1021', 0.00, 'متاح', 'لم يتم البدء');
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `property_types`
+--
+
+CREATE TABLE `property_types` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `property_usage` enum('سكني','تجاري') NOT NULL,
+  `unit_type` varchar(11) DEFAULT NULL,
+  `land_area` decimal(10,2) DEFAULT NULL,
+  `building_area` decimal(10,2) DEFAULT NULL,
+  `floors` int(11) DEFAULT NULL,
+  `bedrooms` int(11) DEFAULT NULL,
+  `halls` int(11) DEFAULT NULL,
+  `bathrooms` int(11) DEFAULT NULL,
+  `kitchen` varchar(255) DEFAULT NULL,
+  `plan_image` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `living_rooms` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- إرجاع أو استيراد بيانات الجدول `property_types`
+--
+
+INSERT INTO `property_types` (`id`, `name`, `property_usage`, `unit_type`, `land_area`, `building_area`, `floors`, `bedrooms`, `halls`, `bathrooms`, `kitchen`, `plan_image`, `created_at`, `living_rooms`) VALUES
+(1, 'A2', 'سكني', 'فيلا', 300.00, 290.00, 2, 4, 2, 5, '1', NULL, '2024-10-02 23:59:21', 0),
+(2, 'A2', 'سكني', 'فيلا', 300.00, 290.00, 2, 4, 2, 5, '1', NULL, '2024-10-02 23:59:27', 0),
+(3, 'A1', 'سكني', 'فيلا', 200.00, 190.00, 2, 4, 1, 4, '1', '66fe8af4da6aa_villa_14 (1).jpg', '2024-10-03 12:15:48', 1);
 
 -- --------------------------------------------------------
 
@@ -357,11 +599,25 @@ INSERT INTO `visits` (`id`, `employee_id`, `visit_date`, `company_name`, `depart
 --
 
 --
+-- Indexes for table `attachments`
+--
+ALTER TABLE `attachments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `client_id` (`client_id`);
+
+--
 -- Indexes for table `calls`
 --
 ALTER TABLE `calls`
   ADD PRIMARY KEY (`id`),
   ADD KEY `employee_id` (`employee_id`);
+
+--
+-- Indexes for table `client_tasks`
+--
+ALTER TABLE `client_tasks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `client_id` (`client_id`);
 
 --
 -- Indexes for table `communication`
@@ -406,6 +662,28 @@ ALTER TABLE `projects`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `project_property_types`
+--
+ALTER TABLE `project_property_types`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `project_id` (`project_id`),
+  ADD KEY `property_type_id` (`property_type_id`);
+
+--
+-- Indexes for table `properties`
+--
+ALTER TABLE `properties`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `project_id` (`project_id`),
+  ADD KEY `property_type_id` (`property_type_id`);
+
+--
+-- Indexes for table `property_types`
+--
+ALTER TABLE `property_types`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `reservations`
 --
 ALTER TABLE `reservations`
@@ -440,9 +718,21 @@ ALTER TABLE `visits`
 --
 
 --
+-- AUTO_INCREMENT for table `attachments`
+--
+ALTER TABLE `attachments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `calls`
 --
 ALTER TABLE `calls`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `client_tasks`
+--
+ALTER TABLE `client_tasks`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
@@ -473,13 +763,31 @@ ALTER TABLE `lost_opportunities`
 -- AUTO_INCREMENT for table `potential_clients`
 --
 ALTER TABLE `potential_clients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `project_property_types`
+--
+ALTER TABLE `project_property_types`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `properties`
+--
+ALTER TABLE `properties`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98;
+
+--
+-- AUTO_INCREMENT for table `property_types`
+--
+ALTER TABLE `property_types`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `reservations`
@@ -510,10 +818,22 @@ ALTER TABLE `visits`
 --
 
 --
+-- قيود الجداول `attachments`
+--
+ALTER TABLE `attachments`
+  ADD CONSTRAINT `attachments_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `customers` (`id`) ON DELETE CASCADE;
+
+--
 -- قيود الجداول `calls`
 --
 ALTER TABLE `calls`
   ADD CONSTRAINT `calls_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `users` (`id`);
+
+--
+-- قيود الجداول `client_tasks`
+--
+ALTER TABLE `client_tasks`
+  ADD CONSTRAINT `client_tasks_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `customers` (`id`) ON DELETE CASCADE;
 
 --
 -- قيود الجداول `communication`
@@ -540,6 +860,20 @@ ALTER TABLE `customers`
 --
 ALTER TABLE `potential_clients`
   ADD CONSTRAINT `potential_clients_ibfk_1` FOREIGN KEY (`assigned_to`) REFERENCES `users` (`id`);
+
+--
+-- قيود الجداول `project_property_types`
+--
+ALTER TABLE `project_property_types`
+  ADD CONSTRAINT `project_property_types_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `project_property_types_ibfk_2` FOREIGN KEY (`property_type_id`) REFERENCES `property_types` (`id`) ON DELETE CASCADE;
+
+--
+-- قيود الجداول `properties`
+--
+ALTER TABLE `properties`
+  ADD CONSTRAINT `properties_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `properties_ibfk_2` FOREIGN KEY (`property_type_id`) REFERENCES `property_types` (`id`) ON DELETE CASCADE;
 
 --
 -- قيود الجداول `reservations`
